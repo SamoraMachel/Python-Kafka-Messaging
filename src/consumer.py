8fdfd2a801ac697a06fb11b8213136be95612674
Samora Machel <samora@me.laptop>
2022-05-17 20:32:08 +0300

    Added group ID in the consumer

--- src/consumer.py
+++ src/consumer.py
@@ -7,18 +7,25 @@ SERVER = "localhost:9092"
 TOPIC = "users"
 
 # Admin configuration
-admin_client : KafkaAdminClient = KafkaAdminClient(bootstrap_servers=SERVER)
-topic_partition : dict = {}
-topic_partition[TOPIC] = NewPartitions(total_count=2)
-admin_client.create_partitions(topic_partition)
+# admin_client : KafkaAdminClient = KafkaAdminClient(bootstrap_servers=SERVER)
+# topic_partition : dict = {}
+# topic_partition[TOPIC] = NewPartitions(total_count=2)
+# admin_client.create_partitions(topic_partition)
 
 
 consumer = KafkaConsumer(
     TOPIC, 
     value_deserializer=lambda x : json.loads(x),
-    auto_offset_reset='earliest'
+    auto_offset_reset='earliest',
+    group_id = "android"
 )
 for msg in consumer:
-    print (msg)
+    print(f"""
+    topic : {msg.topic}
+    partition : {msg.partition}
+    timestamp : {msg.timestamp}
+    value : {msg.value}
+    serialized value size : {msg.serialized_value_size}
+    """)
 metrics = consumer.metrics()
 print(metrics)
