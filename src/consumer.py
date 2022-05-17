17df72eb87e691012c555436ff05eac78cbb40d9
Samora Machel <samora@me.laptop>
2022-05-17 20:13:25 +0300

    Added partitions into the topics

--- src/consumer.py
+++ src/consumer.py
@@ -1,9 +1,22 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaAdminClient
+from kafka.admin import NewPartitions
+from kafka.structs import TopicPartition
 import json
 
+SERVER = "localhost:9092"
+TOPIC = "users"
+
+# Admin configuration
+admin_client : KafkaAdminClient = KafkaAdminClient(bootstrap_servers=SERVER)
+topic_partition : dict = {}
+topic_partition[TOPIC] = NewPartitions(total_count=2)
+admin_client.create_partitions(topic_partition)
+
+
 consumer = KafkaConsumer(
-    'users', 
-    value_deserializer=lambda x : json.loads(x)
+    TOPIC, 
+    value_deserializer=lambda x : json.loads(x),
+    auto_offset_reset='earliest'
 )
 for msg in consumer:
     print (msg)
--- src/producer.py
+++ src/producer.py
@@ -1,16 +1,25 @@
 from kafka import KafkaProducer
 import json
 
+TOPIC = 'users'
+
+
+
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8'),
     compression_type='gzip'
 )
 user_input : str = ""
+partition = 0
 
 while user_input != "q":
     user_input : str = input("Enter a message : ")
-    producer.send('users', {"input": user_input})
+    if user_input.capitalize()[0] < 'N':
+        partition = 0
+    else:
+        partition = 1
+    producer.send(TOPIC, {"input": user_input}, partition=partition)
     if user_input == 'q':
         break
     else:
